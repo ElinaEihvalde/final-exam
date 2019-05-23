cc3aa573ad8e9c25b28ed0f2a6907c6e0bfe8951
ElinaEihvalde <38621946+ElinaEihvalde@users.noreply.github.com>
2019-05-23 20:11:23 +0300

    I commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,8 +6,7 @@
       
       <h3>STARGAZING EXPERIENCE</h3>
     </div>
-    <img class="observatory" src="../assets/obesrvatorija.png" 
-    >
+
     
   </div>
  
@@ -22,14 +21,29 @@
   }
 </script>
 
-<style <style scoped>
+ <style scoped>
 
 
-.home {height: 100vh; z-index: -99; background-image: url('../assets/home-backgr2.png');}
-.banner-txt {position: relative;z-index: 12; color: #fff; width: 45%; top: 150px; left:64px; }
-.banner-txt h1 {font-size: 3.5em; line-height: 1.3;}
-.banner-txt h5 {font-size: 1.5em; font-weight: 300;}
-/* .color {color: teal;} */
+.home {height: 100vh;  background-image: url('../assets/home-backgr.jpg'); background-size: 100%;}
+.banner-txt {position: relative; color: #fff; text-align: center; top: 250px; animation-name: fadeIn; animation-duration: 3s; }
+
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+.banner-txt h1 {font-size: 5vw; line-height: 1.3;  }
+
+
+
+.banner-txt h3 {font-size: 1.5em; font-weight: 300; color: #E2BA36;}
+
+
+
 .line {width: 100%; margin-top: 20px; border-bottom: 3px solid #fff;z-index: 11;}
 .observatory {position: fixed; width: 90%; height: 100%; z-index: 10; right: 0; bottom: 0; top: 50px; opacity: 0.8;}
 .body { background-color: #030309; height: 500px;}
