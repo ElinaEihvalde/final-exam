c03bc4200e032be23142a561fecf8305927e2d6b
GroovyQ187 <eeriksrancaans@gmail.com>
2019-05-31 15:10:01 +0300

    Home
    Home

--- src/components/full-width-seperator.vue
+++ src/components/full-width-seperator.vue
@@ -1,20 +1,21 @@
 <template>
-  <v-container fluid pa-0 class="seperator-background">
-    <v-layout align-center justify-center grid-list-xl class="seperator" py-5 data-aos="fade-up" data-aos-duration="1000" px-20>
-          <v-flex xs4 v-bind:class="info.class" py-5 v-for="info in infos" :key="info.id">
-            <v-flex class="info-img" data-aos="fade-up" data-aos-duration="1500">
+  <v-container fluid pa-0 grid-list-xxl>
+    <v-layout align-center justify-center class="seperator" py-5 data-aos="fade-up" data-aos-duration="1000">
+      <v-flex xs10>
+        <v-layout row>
+          <v-flex xs4 v-bind:class="info.class" py-5 v-for="info in infos" :key="info.id"  class="info-img">
             <v-img  v-bind:src="info.pic" class="info-pic mb-4 ml-5 align-center">
             </v-img>
-            </v-flex>
-            <v-flex>
             <h5 class="display-1">{{info.title}}</h5>
             <div class="line" ></div>
             <p class="body">{{info.content}}</p>
           </v-flex>
+        </v-layout>
           </v-flex>
         </v-layout>
   </v-container>
 </template>
+
 
 <script>
 /* import quote from './quote'  */
@@ -29,13 +30,13 @@ export default {
                     title: 'OBSERVATORY',
                     content: 'Lorem ipsum dolor sit amet, consectetur voluptatibus rerum error soluta at accusantium? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt mollitia, eos architecto alias consequuntur voluptatibus rerum error soluta at accusantium?',
                     pic: require('@/assets/telescope.svg'),
+                    
                 },
                 {
                     id: 2,
                     title: 'GUESTHOUSE',
                     content: 'Lorem ipsum dolor sit amet, consectetur voluptatibus rerum error soluta at accusantium? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt mollitia, eos architecto alias consequuntur voluptatibus rerum error soluta at accusantium?',
                     pic: require('@/assets/house.svg'),
-                    class: 'mx-5',
                 },
                {
                     id: 3,
@@ -57,7 +58,6 @@ export default {
   height: 60vh;
 }
 .seperator-background {
-  background-color: #050505;
 }
 
 .info-pic {max-width: 20%;}
