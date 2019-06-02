6edef8e0e9dcb375d8d330e8ecefd85881798cad
ElinaEihvalde <38621946+ElinaEihvalde@users.noreply.github.com>
2019-06-02 22:21:00 +0300

    Revert "Revert "updated about page""
    This reverts commit 4b10b8d83561e88b394c88f02b7ce621dfc2559b.

--- src/components/visit/slider-observatory.vue
+++ src/components/visit/slider-observatory.vue
@@ -1,5 +1,21 @@
 <template>
- <v-carousel hide-delimiters>
+<!-- <v-container fluid>
+    <v-layout >
+        <v-flex xs12>
+            <v-container fluid grid-list-xl>
+              <v-layout row wrap>
+                <v-flex  v-bind="{ [`xs${item.flex}`]: true }" v-for="(item,i) in items" :key="i">
+                    <v-img  :src="item.src" contain> </v-img>
+                </v-flex>
+    </v-layout>
+    
+      
+    </v-container>
+     </v-flex>
+    </v-layout>
+</v-container> -->
+<v-carousel hide-delimiters hide-controls
+ elevation-11>
     <v-carousel-item
       v-for="(item,i) in items"
       :key="i"
@@ -9,18 +25,25 @@
 </template>
 
 <script>
-
 export default {
-  data() {
-    return {
-      items: [
-          {src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'},
-          {src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'},
-          {src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'},
-          {src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'}
-        ]
-    };
-  }
+    data() {
+        return {
+            items: [{
+                    src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+                    flex: 6
+                },
+                {
+                    src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+                    flex: 6
+                },
+                {
+                    src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+                    flex: 12
+                },
+
+            ]
+        };
+    }
 };
 </script>
 
