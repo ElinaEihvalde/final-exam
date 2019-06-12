194e226fa0475df11a39439da75844a5efe080c1
GroovyQ187 <eeriksrancaans@gmail.com>
2019-06-12 14:52:30 +0300

    New database
    NEW

--- src/main.js
+++ src/main.js
@@ -19,13 +19,20 @@ new Vue({
   created () {
     AOS.init(),
     firebase.initializeApp({
-      apiKey: "AIzaSyC6m02qmkf_0zNwBR8LobblU-2yWKY_zMk",
+      apiKey: "AIzaSyDdTg8G5mQSAwQUIHzgekArSORbFcCRMZs",
+      authDomain: "lielzeltiniv2.firebaseapp.com",
+      databaseURL: "https://lielzeltiniv2.firebaseio.com",
+      projectId: "lielzeltiniv2",
+      storageBucket: "lielzeltiniv2.appspot.com",
+      messagingSenderId: "917993463692",
+      appId: "1:917993463692:web:a146cc662fa5176b"
+/*       apiKey: "AIzaSyC6m02qmkf_0zNwBR8LobblU-2yWKY_zMk",
     authDomain: "lielzeltini-80359.firebaseapp.com",
     databaseURL: "https://lielzeltini-80359.firebaseio.com",
     projectId: "lielzeltini-80359",
     storageBucket: "lielzeltini-80359.appspot.com",
     messagingSenderId: "606113283110",
-    appId: "1:606113283110:web:b73e9936d6c4464d"
+    appId: "1:606113283110:web:b73e9936d6c4464d" */
     })
     firebase.auth().onAuthStateChanged((user) =>{
       if (user) {
