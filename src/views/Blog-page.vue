726419c0b9fb8d5d979b37c8f9f0a8638a418d1c
ElinaEihvalde <38621946+ElinaEihvalde@users.noreply.github.com>
2019-06-15 09:33:18 +0300

    back to top btn

--- src/views/Blog-page.vue
+++ src/views/Blog-page.vue
@@ -1,27 +1,13 @@
 <template>
-
 <v-container fluid>
-    
-    <blogTitle/>
-      <v-btn
-  class="md-5 mr-3  elevation-2"
- 
-  
-  flat
-  icon
- bottom
-  right
-  color="#050505"
-  fixed
-  @click="top"
->
-<v-icon>arrow_upward</v-icon>
+
+    <blogTitle />
+    <v-btn class="md-5 mr-3  elevation-2" :class="{show: scrollPosition > 300, hide: scrollPosition < 300}" flat icon bottom right color="#050505" fixed @click="top">
+        <v-icon>arrow_upward</v-icon>
     </v-btn>
-    <blogPosts/>
-    
-  
+    <blogPosts />
+
 </v-container>
-    
 </template>
 
 <script>
@@ -33,19 +19,57 @@ export default {
         blogPosts
     },
 
-    methods:{
-      top(){
-            window.scrollTo({
-  top: 0,
-  left: 0,
-  behavior: 'smooth'
-})
-           }
+    data() {
+
+        return {
+            scrollPosition: null,
+            hide: false,
+            show: true
         }
-    
+    },
+
+    methods: {
+
+        updateScroll() {
+            this.scrollPosition = window.scrollY
+        },
+        top() {
+            window.scrollTo({
+                top: 0,
+                left: 0,
+                behavior: 'smooth'
+            })
+        }
+    },
+
+    mounted() {
+    window.addEventListener('scroll', this.updateScroll);
+  }
+
 }
 </script>
 
 <style>
+.hide {
+    display: none;
 
+}
+
+
+.show {
+    animation-name: fadeInBtn;
+    animation-duration: 0.5s;
+    transition: ease-in-out;
+}
+
+@keyframes fadeInBtn {
+    0% {
+        opacity: 0;
+
+    }
+
+    100% {
+        opacity: 1;
+    }
+}
 </style>
